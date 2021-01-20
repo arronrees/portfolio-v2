eb76adc85159b0e6969d62062b88c4143efb23e3
arronrees <64908445+arronrees@users.noreply.github.com>
2021-01-20 15:38:51 +0000

    Added logo animation

---
+++ app.js
@@ -0,0 +1,37 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const logoText = document.querySelector('.logo');
+const logoTextContent = logoText.textContent;
+const logoSplitText = logoTextContent.split('');
+logoText.textContent = '';
+
+// Slides letters in from 50px
+function logoAnimation() {
+  logoSplitText.forEach((letter) => {
+    // Adds space for multiple words
+    if (letter === ' ') {
+      logoText.innerHTML += `<span class="logo-span" style="width: 1rem">${letter}</span>`;
+    } else {
+      logoText.innerHTML += `<span class="logo-span">${letter}</span>`;
+    }
+  });
+
+  const int = setInterval(logoLetterAnim, 50);
+  let char = 0;
+
+  // Adds css class to each letter and clears interval when done
+  function logoLetterAnim() {
+    const span = logoText.querySelectorAll('.logo-span')[char];
+    span.classList.add('logo-text-fade');
+    char++;
+
+    if (char === logoSplitText.length) {
+      clearInterval(int);
+      return;
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  logoAnimation();
+});
