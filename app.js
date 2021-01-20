38284ee2a7e2312362b99af583c31692ee79b6da
arronrees <64908445+arronrees@users.noreply.github.com>
2021-01-20 17:29:27 +0000

    Added scroll trigger for projects images and text

--- app.js
+++ app.js
@@ -37,11 +37,10 @@ const heroImgMask = document.querySelector('.hero-img-mask');
 const heroText = document.querySelectorAll('.hero-text');
 const heroTextMask = document.querySelectorAll('.hero-text-mask');
 
+// Sets mask and item opposites on y axis
+gsap.set([heroText, heroImg], { yPercent: 105 });
+gsap.set([heroImgMask, heroTextMask], { yPercent: -101 });
 function heroEnterAnimation() {
-  // Sets mask and item opposites on y axis
-  gsap.set([heroText, heroImg], { yPercent: 105 });
-  gsap.set([heroImgMask, heroTextMask], { yPercent: -101 });
-
   const tl = gsap.timeline({
     defaults: {
       duration: 1,
@@ -54,7 +53,65 @@ function heroEnterAnimation() {
     yPercent: 0,
   }).to([heroImg, heroImgMask], {
     yPercent: 0,
-    onComplete: logoAnimation,
+    onComplete: () => {
+      logoAnimation();
+      projectsEnterAnimation();
+    },
+  });
+}
+
+const projectTextH = document.querySelectorAll('.project-text-h');
+const projectTextHMask = document.querySelectorAll('.project-text-h-mask');
+const projectTextP = document.querySelectorAll('.project-text-p');
+const projectTextPMask = document.querySelectorAll('.project-text-p-mask');
+
+const projectImage = document.querySelectorAll('.project-img');
+const projectImageMask = document.querySelectorAll('.project-img-mask');
+
+// Sets mask and item opposites on y axis
+gsap.set([projectImage, projectTextHMask, projectTextPMask], { yPercent: 105 });
+gsap.set([projectTextP, projectTextH, projectImageMask], { yPercent: -101 });
+
+function projectsEnterAnimation() {
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.7,
+      ease: 'power1.out',
+    },
+  });
+
+  // creates scroll trigger for each grid image
+  projectImage.forEach((img, index) => {
+    ScrollTrigger.create({
+      trigger: img,
+      start: 'top 90%',
+      onEnter: () => {
+        tl.to([img, projectImageMask[index]], {
+          yPercent: 0,
+        });
+      },
+    });
+  });
+
+  // creates scroll trigger for each grid text
+  projectTextH.forEach((text, index) => {
+    ScrollTrigger.create({
+      trigger: text,
+      start: 'top 70%',
+      onEnter: () => {
+        tl.to(
+          [
+            text,
+            projectTextHMask[index],
+            projectTextP[index],
+            projectTextPMask[index],
+          ],
+          {
+            yPercent: 0,
+          }
+        );
+      },
+    });
   });
 }
 
